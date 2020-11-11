21077cf78e02c119b7cf8768a65e274d0d5f2f4a
okothkongo <okothkongo@gmail.com>
2020-11-12 00:47:35 +0300

    real time connectivity

--- apps/auction_web/lib/auction_web/views/item_view.ex
+++ apps/auction_web/lib/auction_web/views/item_view.ex
@@ -1,3 +1,11 @@
 defmodule AuctionWeb.ItemView do
   use AuctionWeb, :view
+
+  def is_bid_still_open?(end_time) do
+    date_diff = DateTime.diff(DateTime.utc_now(), end_time)
+
+    if date_diff >= 0 do
+      true
+    end
+  end
 end
